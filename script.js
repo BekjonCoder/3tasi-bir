12b4c276c413b41c0d447e323ff6ba2904856252
BekjonCoder <k75154256@gmail.com>
2025-02-22 23:48:19 +0500

    fff

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const date=new Date()
-const date=new Date('2025-02-06T19:00:00')
-const date=new Date()
-let year=date.getFullYear()
-let month=date.getMonth()
-let day=date.getDay()
-let hour=date.getHours()
-let minute=date.getMinutes()
-let secund=date.getSeconds()
+// const date=new Date()
+// const date=new Date('2025-02-06T19:00:00')
+// const date=new Date()
+// let year=date.getFullYear()
+// let month=date.getMonth()
+// let day=date.getDay()
+// let hour=date.getHours()
+// let minute=date.getMinutes()
+// let secund=date.getSeconds()
 
 
-console.log(year,month,day,hour,minute,secund);
+// console.log(year,month,day,hour,minute,secund);
 
