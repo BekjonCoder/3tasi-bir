a7322c40b3aee82c0426e6f5a84ae5f19ac29ed6
BekjonCoder <k75154256@gmail.com>
2025-02-23 09:53:39 +0500

    keyin va oldin

--- script.js
+++ script.js
@@ -19,3 +19,22 @@
 // let date=new Date()
 // console.log(date.toLocaleTimeString('ru-RU'));
 
+// let date=new Date(prompt("Sanani kriting"))
+// let day=date.getDay()
+// let keyin=day+7;
+// let oldin=day-7;
+// let d=new Date()
+// let s=new Date()
+// d.setDate(oldin)
+// s.setDate(keyin)
+// console.log(d,s);
+
+let date=new Date(prompt("Yil-oy-sana"))
+
+let oldin=new Date(date)
+let keyin=new Date(date)
+oldin.setDate(date.getDate()-7)
+keyin.setDate(date.getDate()+7)
+console.log('7 kun keyin'+oldin.toDateString());
+console.log('7 kun oldin'+keyin.toDateString());
+
