7d567028a7ba5e40a50984a85df3cb90dc928339
BekjonCoder <k75154256@gmail.com>
2025-02-23 08:59:36 +0500

    fff

--- script.js
+++ script.js
@@ -8,12 +8,12 @@
 // let minute=date.getMinutes()
 // let secund=date.getSeconds()
 // console.log(year,month,day,hour,minute,secund);
-// const date=new Date()
-// date.setFullYear(2024)
-// date.setMonth(5)
-// date.setDate(15)
-// date.setHours(12)
-// date.setMinutes(59)
-// date.setSeconds(59)
+const date=new Date()
+date.setFullYear(2024)
+date.setMonth(5)
+date.setDate(15)
+date.setHours(12)
+date.setMinutes(59)
+date.setSeconds(59)
 document.write(date);
 
